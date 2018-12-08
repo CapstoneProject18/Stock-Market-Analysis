1bf51fc5d128ab3e4c546e6e5cc9045f0e107767
mayank singh <mayank.singh0597@gmail.com>
2018-12-08 23:46:57 +0530

    integration of comparision module

--- visualization/myapp/urls.py
+++ visualization/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- 	path('', views.chart, name='chart'),
- 	path('fetch/', views.fetch, name='fetch'),
- 	path('company/', views.fetch, name='company')	
+ 	path(r'', views.fetch_compare, name='fetch'),
+ 	path(r'fetch/', views.fetch_compare, name='fetch_compare'),
+ 	path(r'company/', views.fetch_compare, name='company_compare')	
  ]
--- visualization/myapp/views.py
+++ visualization/myapp/views.py
@@ -8,7 +8,7 @@ class NameForm(forms.Form):
     start_date = forms.CharField(label='start_date', max_length=100)
     end_date = forms.CharField(label='end_date', max_length=100)
 
-def fetch(request):
+def fetch_compare(request):
     if request.method == 'POST':
         company = []
         company1 = ''
